ba222b7a70363f12bdc4c727260b7ec9ce2b0b2e
berenger-bramas <berenger-bramas@2616d619-271b-44dc-8df4-d4a8f33a7222>
2011-05-17 06:30:14 +0000

    Add a file to compute some stats about the tree & particles. (Repartition etc.)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/scalfmm/scalfmm/trunk@91 2616d619-271b-44dc-8df4-d4a8f33a7222

--- Tests/testFmbAlgorithm.cpp
+++ Tests/testFmbAlgorithm.cpp
@@ -147,7 +147,6 @@ int main(int argc, char ** argv){
         std::cout << "Potential = " << potential << std::endl;
     }
 
-
     // -----------------------------------------------------
 
     std::cout << "Deleting particles ..." << std::endl;
