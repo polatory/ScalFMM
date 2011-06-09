15e752251aac65b4459676743cfb0cb9e951a35f
berenger-bramas <berenger-bramas@2616d619-271b-44dc-8df4-d4a8f33a7222>
2011-06-09 15:46:57 +0000

    Nothing git svn problem.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/scalfmm/scalfmm/trunk@123 2616d619-271b-44dc-8df4-d4a8f33a7222

--- Tests/testFmmAlgorithm.cpp
+++ Tests/testFmmAlgorithm.cpp
@@ -40,7 +40,7 @@ int main(int argc, char ** argv){
 
     const int NbLevels = 6;//10;
     const int SizeSubLevels = 3;//3
-    const long NbPart = 20000;//2000000
+    const long NbPart = 2000000;//2000000
 
     FTic counter;
 
