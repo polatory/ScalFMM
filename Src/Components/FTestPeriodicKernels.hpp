7f187550b53ac295a1ab881442dbd125da08e9a5
berenger-bramas <berenger-bramas@2616d619-271b-44dc-8df4-d4a8f33a7222>
2012-01-24 15:30:57 +0000

    Put the test periodic kernel in a file.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/scalfmm/scalfmm/trunk@308 2616d619-271b-44dc-8df4-d4a8f33a7222

---
+++ Src/Components/FTestPeriodicKernels.hpp
@@ -0,0 +1,45 @@
+#ifndef FTESTPERIODICKERNELS_HPP
+#define FTESTPERIODICKERNELS_HPP
+
+#include "FTestKernels.hpp"
+
+/** This kernel is the periodic version of the test kernel
+  */
+
+template< class ParticleClass, class CellClass, class ContainerClass>
+class FTestPeriodicKernels : public FTestKernels<ParticleClass,CellClass,ContainerClass> {
+public:
+
+    /** Before Downward */
+    void M2L(CellClass* const FRestrict pole, const CellClass* distantNeighbors[189], const FTreeCoordinate [189], const int size, const int ) {
+        // The pole is impacted by what represent other poles
+        for(int idx = 0 ; idx < size ; ++idx){
+            pole->setDataDown(pole->getDataDown() + distantNeighbors[idx]->getDataUp());
+        }
+    }
+
+
+    /** After Downward */
+    void P2P(const MortonIndex ,
+             ContainerClass* const FRestrict targets, const ContainerClass* const FRestrict sources,
+             ContainerClass* const directNeighborsParticles[26], const FTreeCoordinate [26], const int size) {
+
+        // Each particles targeted is impacted by the particles sources
+        long long int inc = sources->getSize();
+        if(targets == sources){
+            inc -= 1;
+        }
+        for(int idx = 0 ; idx < size ; ++idx){
+            inc += directNeighborsParticles[idx]->getSize();
+        }
+
+        typename ContainerClass::BasicIterator iter(*targets);
+        while( iter.hasNotFinished() ){
+            iter.data().setDataDown(iter.data().getDataDown() + inc);
+            iter.gotoNext();
+        }
+
+    }
+};
+
+#endif // FTESTPERIODICKERNELS_HPP
