df17b9ce41cd94fa0e8a37fc96325c4f21c62f70
berenger-bramas <berenger-bramas@2616d619-271b-44dc-8df4-d4a8f33a7222>
2012-02-02 15:04:04 +0000

    Update the cmake (and the blas file)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/scalfmm/scalfmm/trunk@342 2616d619-271b-44dc-8df4-d4a8f33a7222

--- Src/Utils/FBlas.hpp
+++ Src/Utils/FBlas.hpp
@@ -12,6 +12,8 @@
 
 #include "FGlobal.hpp"
 
+
+
 #ifdef SCALFMM_USE_CBLAS
 
     #ifdef  SCALFMM_USE_MKL_AS_BLAS
@@ -19,24 +21,43 @@
     #else
     #include <cblas.h>
     #endif
+#else
+    enum CBLAS_ORDER {CblasRowMajor=101, CblasColMajor=102};
+    enum CBLAS_TRANSPOSE {CblasNoTrans, CblasTrans, CblasConjTrans};
+#endif
 
+template <typename T>
+void cblas_gemv(const CBLAS_ORDER order ,
+                   const CBLAS_TRANSPOSE TransA , const int M , const int N ,
+                   const void *alpha , const void *A , const int lda ,
+                   const void *X , const int incX , const void *beta ,
+                   void *Y , const int incY){
+}
+template <typename T>
+void cblas_dotu_sub( const int N , const void *X , const int incX ,
+                     const void *Y , const int incY , void *dotu){
+}
+
+#ifdef SCALFMM_USE_CBLAS
     ///////////////////////////////////////////////////////
     // GEMV
     ///////////////////////////////////////////////////////
 
-    void cblas_gemv(const CBLAS_ORDER order ,
-            const CBLAS_TRANSPOSE TransA , const int M , const int N ,
-            const double *alpha , const double *A , const int lda ,
-            const double *X , const int incX , const double *beta ,
-            double *Y , const int incY){
+    template <>
+    void cblas_gemv<double>(const CBLAS_ORDER order ,
+                            const CBLAS_TRANSPOSE TransA , const int M , const int N ,
+                            const void *alpha , const void *A , const int lda ,
+                            const void *X , const int incX , const void *beta ,
+                            void *Y , const int incY){
         cblas_zgemv(order,TransA,M,N,alpha,A,lda,X,incX,beta,Y,incY);
     }
 
-    void cblas_gemv(const CBLAS_ORDER order ,
-            const CBLAS_TRANSPOSE TransA , const int M , const int N ,
-            const float *alpha , const float *A , const int lda ,
-            const float *X , const int incX , const float *beta ,
-            float *Y , const int incY){
+    template <>
+    void cblas_gemv<float>(const CBLAS_ORDER order ,
+                           const CBLAS_TRANSPOSE TransA , const int M , const int N ,
+                           const void *alpha , const void *A , const int lda ,
+                           const void *X , const int incX , const void *beta ,
+                           void *Y , const int incY){
         cblas_cgemv(order,TransA,M,N,alpha,A,lda,X,incX,beta,Y,incY);
     }
 
@@ -45,33 +66,21 @@
     // Dotu
     ///////////////////////////////////////////////////////
 
-    void cblas_dotu_sub(const int N , const double *X , const int incX ,
-                            const double *Y , const int incY , double *dotu){
+    template <>
+    void cblas_dotu_sub<double>(const int N , const void *X , const int incX ,
+                            const void *Y , const int incY , void *dotu){
         cblas_zdotu_sub(N,X,incX,Y,incY,dotu);
     }
 
-    void cblas_dotu_sub(const int N , const float *X , const int incX ,
-                           const float *Y , const int incY , float *dotu){
+    template <>
+    void cblas_dotu_sub<float>(const int N , const void *X , const int incX ,
+                           const void *Y , const int incY , void *dotu){
         cblas_cdotu_sub(N,X,incX,Y,incY,dotu);
     }
 
-#else
-    enum CBLAS_ORDER {CblasRowMajor=101, CblasColMajor=102};
-    enum CBLAS_TRANSPOSE {CblasNoTrans, CblasTrans, CblasConjTrans};
-
-    template <typename T>
-    void cblas_gemv(const CBLAS_ORDER order ,
-                       const CBLAS_TRANSPOSE TransA , const int M , const int N ,
-                       const void *alpha , const void *A , const int lda ,
-                       const void *X , const int incX , const void *beta ,
-                       void *Y , const int incY){
-    }
-    template <typename T>
-    void cblas_dotu_sub( const int N , const void *X , const int incX ,
-                         const void *Y , const int incY , void *dotu){
-    }
-
 #endif //SCALFMM_USE_CBLAS
+
+
 
 #endif //FBLAS_HPP
 
