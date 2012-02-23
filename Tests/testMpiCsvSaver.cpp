098e7285f82ffbc92656173ae0e22f258067e8c0
berenger-bramas <berenger-bramas@2616d619-271b-44dc-8df4-d4a8f33a7222>
2012-02-23 09:49:29 +0000

    Create parallel mpi csv file saver.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/scalfmm/scalfmm/trunk@430 2616d619-271b-44dc-8df4-d4a8f33a7222

---
+++ Tests/testMpiCsvSaver.cpp
@@ -0,0 +1,118 @@
+
+// ===================================================================================
+// Logiciel initial: ScalFmm Version 0.5
+// Co-auteurs : Olivier Coulaud, Bérenger Bramas.
+// Propriétaires : INRIA.
+// Copyright © 2011-2012, diffusé sous les termes et conditions d’une licence propriétaire.
+// Initial software: ScalFmm Version 0.5
+// Co-authors: Olivier Coulaud, Bérenger Bramas.
+// Owners: INRIA.
+// Copyright © 2011-2012, spread under the terms and conditions of a proprietary license.
+// ===================================================================================
+
+#include <iostream>
+
+#include <cstdio>
+#include <cstdlib>
+
+#include "../Src/Utils/FTic.hpp"
+#include "../Src/Utils/FParameters.hpp"
+#include "../Src/Utils/FMpi.hpp"
+
+#include "../Src/Containers/FOctree.hpp"
+#include "../Src/Containers/FVector.hpp"
+
+#include "../Src/Extensions/FExtendVelocity.hpp"
+
+#include "../Src/Files/FTreeMpiCsvSaver.hpp"
+#include "../Src/Files/FFmaLoader.hpp"
+#include "../Src/Arranger/FOctreeArranger.hpp"
+
+#include "../Src/Components/FSimpleLeaf.hpp"
+#include "../Src/Components/FBasicCell.hpp"
+
+#include "../Src/Kernels/FSphericalParticle.hpp"
+
+class FmmVeloParticle : public FSphericalParticle, public FExtendVelocity {
+};
+
+template <class ParticleClass>
+class GalaxyLoader : public FFmaLoader<ParticleClass> {
+public:
+    GalaxyLoader(const char* const filename) : FFmaLoader<ParticleClass>(filename) {
+    }
+
+    void fillParticle(ParticleClass& inParticle){
+        FReal x,y,z,data, vx, vy, vz;
+        this->file >> x >> y >> z >> data >> vx >> vy >> vz;
+        inParticle.setPosition(x,y,z);
+        inParticle.setPhysicalValue(data);
+        inParticle.setVelocity(vx,vy,vz);
+        inParticle.setPhysicalValue(FReal(0.10));
+    }
+};
+
+
+template <class OctreeClass, class ContainerClass , class ParticleClass>
+class MassSaver : public FTreeMpiCsvSaver<OctreeClass,ContainerClass, ParticleClass> {
+public:
+    MassSaver(const char inBasefile[], const FMpi::FComm& inComm, const bool inIncludeHeader = false)
+        : FTreeMpiCsvSaver<OctreeClass,ContainerClass, ParticleClass>(inBasefile, inComm, inIncludeHeader) {
+    }
+
+    virtual FReal getValue(ParticleClass*const part){
+        return part->getPhysicalValue();
+    }
+};
+
+// Simply create particles and try the kernels
+int main(int argc, char ** argv){
+    typedef FmmVeloParticle         ParticleClass;
+    typedef FBasicCell              CellClass;
+    typedef FVector<ParticleClass>  ContainerClass;
+
+    typedef FSimpleLeaf<ParticleClass, ContainerClass >                     LeafClass;
+    typedef FOctree<ParticleClass, CellClass, ContainerClass , LeafClass >  OctreeClass;
+    ///////////////////////What we do/////////////////////////////
+    std::cout << ">> This executable has to be used to test Spherical algorithm.\n";
+    //////////////////////////////////////////////////////////////
+    FMpi app( argc, argv);
+
+    const int NbLevels = FParameters::getValue(argc,argv,"-h", 6);
+    const int SizeSubLevels = FParameters::getValue(argc,argv,"-sh", 3);
+
+    GalaxyLoader<ParticleClass> loader(FParameters::getStr(argc,argv,"-f", "../Data/galaxy.fma.tmp"));
+
+    // -----------------------------------------------------
+
+    OctreeClass tree(NbLevels, SizeSubLevels, loader.getBoxWidth(), loader.getCenterOfBox());
+
+    // -----------------------------------------------------
+
+    std::cout << "Creating & Inserting " << loader.getNumberOfParticles() << " particles ..." << std::endl;
+    std::cout << "\tHeight : " << NbLevels << " \t sub-height : " << SizeSubLevels << std::endl;
+
+    {
+        ParticleClass particleToFill;
+        for(int idxPart = 0 ; idxPart < loader.getNumberOfParticles() ; ++idxPart){
+            loader.fillParticle(particleToFill);
+            if( (idxPart+1) % (app.global().processId()+1) == 0) tree.insert(particleToFill);
+        }
+    }
+
+    // -----------------------------------------------------
+
+    {
+        MassSaver<OctreeClass, ContainerClass, ParticleClass> saver("./out/test%d.csv", app.global() , false);
+        saver.exportTree(&tree);
+    }
+
+    // -----------------------------------------------------
+
+    {
+        MassSaver<OctreeClass, ContainerClass, ParticleClass> saver("./out/htest%d.csv", app.global() , true);
+        saver.exportTree(&tree);
+    }
+
+    return 0;
+}
