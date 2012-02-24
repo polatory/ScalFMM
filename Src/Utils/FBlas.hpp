92d240bce52db68c50299fb9df0787bb311a4a7f
berenger-bramas <berenger-bramas@2616d619-271b-44dc-8df4-d4a8f33a7222>
2012-02-24 16:16:44 +0000

    Remove the cblas interface in the blas file.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/scalfmm/scalfmm/trunk@441 2616d619-271b-44dc-8df4-d4a8f33a7222

--- Src/Utils/FBlas.hpp
+++ Src/Utils/FBlas.hpp
@@ -11,72 +11,13 @@
 #ifndef FBLAS_HPP
 #define FBLAS_HPP
 
+#include "FGlobal.hpp"
+
 // This file interfaces the blas functions
 // to enable a generic use.
 // If no blas has been enabled in the cmake,
 // the function will be empty
 
-///////////////////////////////////////////////////////
-// Manage Blas Version
-///////////////////////////////////////////////////////
-
-#include "FGlobal.hpp"
-
-
-
-//#ifdef SCALFMM_USE_CBLAS
-//
-//#ifdef  SCALFMM_USE_MKL_AS_BLAS
-//#include <mkl_cblas.h>
-//#else
-//#include <cblas.h>
-//#include <clapack.h>
-//#endif
-//#else
-//    enum CBLAS_ORDER {CblasRowMajor=101, CblasColMajor=102};
-//    enum CBLAS_TRANSPOSE {CblasNoTrans, CblasTrans, CblasConjTrans};
-//#endif
-//
-/////////////////////////////////////////////////////////
-//// GEMV
-/////////////////////////////////////////////////////////
-//
-//void cblas_gemv(const CBLAS_ORDER order ,
-//								const CBLAS_TRANSPOSE TransA , const int M , const int N ,
-//								const double *alpha , const double *A , const int lda ,
-//								const double *X , const int incX , const double *beta ,
-//								double *Y , const int incY){
-//	cblas_zgemv(order,TransA,M,N,alpha,A,lda,X,incX,beta,Y,incY);
-//}
-//
-//void cblas_gemv(const CBLAS_ORDER order ,
-//								const CBLAS_TRANSPOSE TransA , const int M , const int N ,
-//								const float *alpha , const float *A , const int lda ,
-//								const float *X , const int incX , const float *beta ,
-//								float *Y , const int incY){
-//	cblas_cgemv(order,TransA,M,N,alpha,A,lda,X,incX,beta,Y,incY);
-//}
-//
-//
-/////////////////////////////////////////////////////////
-//// Dotu
-/////////////////////////////////////////////////////////
-//
-//void cblas_dotu_sub(const int N , const double *X , const int incX ,
-//										const double *Y , const int incY , double *dotu){
-//	cblas_zdotu_sub(N,X,incX,Y,incY,dotu);
-//}
-//
-//void cblas_dotu_sub(const int N , const float *X , const int incX ,
-//										const float *Y , const int incY , float *dotu){
-//	cblas_cdotu_sub(N,X,incX,Y,incY,dotu);
-//}
-
-
-//////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////
 
 // for real
 const double D_ZERO =  0.0;
