ebd2517450c7332e0c50df444b7f579d2b030743
Berenger Bramas <berenger.bramas@inria.fr>
2012-03-20 13:58:56 +0100

    Add a starpu example with the spĥerical kernel

---
+++ Tests/Kernels/testStarpuSphericalAlgorithm.cpp
@@ -0,0 +1,129 @@
+// ===================================================================================
+// Logiciel initial: ScalFmm Version 0.5
+// Co-auteurs : Olivier Coulaud, Bérenger Bramas.
+// Propriétaires : INRIA.
+// Copyright © 2011-2012, diffusé sous les termes et conditions d’une licence propriétaire.
+// Initial software: ScalFmm Version 0.5
+// Co-authors: Olivier Coulaud, Bérenger Bramas.
+// Owners: INRIA.
+// Copyright © 2011-2012, spread under the terms and conditions of a proprietary license.
+// ===================================================================================
+
+// ==== CMAKE =====
+// @FUSE_STARPU
+// ================
+
+#include <starpu.h>
+
+
+#include "../../Src/Utils/FTic.hpp"
+#include "../../Src/Utils/FParameters.hpp"
+
+#include "../../Src/Containers/FOctree.hpp"
+#include "../../Src/Containers/FVector.hpp"
+
+#include "../../Src/Core/FFmmAlgorithmStarpu.hpp"
+
+#include "../../Src/Components/FSimpleLeaf.hpp"
+
+#include "../../Src/Kernels/Spherical/FSphericalKernel.hpp"
+#include "../../Src/Kernels/Spherical/FSphericalCell.hpp"
+#include "../../Src/Kernels/Spherical/FSphericalParticle.hpp"
+
+#include "../../Src/Files/FFmaLoader.hpp"
+
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+////////////////////////////////////////////////////////////////
+// Typedefs
+////////////////////////////////////////////////////////////////
+typedef FSphericalParticle             ParticleClass;
+typedef StarVector<ParticleClass> ContainerClass;
+typedef DataVector<ParticleClass> RealContainerClass;
+
+typedef FSphericalCell                RealCellClass;
+typedef FStarCell<RealCellClass> CellClass;
+
+
+typedef FSimpleLeaf<ParticleClass, ContainerClass >                     LeafClass;
+typedef FOctree<ParticleClass, CellClass, ContainerClass , LeafClass >  OctreeClass;
+
+typedef FSphericalKernel<ParticleClass, RealCellClass, RealContainerClass >          KernelClass;
+
+typedef FFmmAlgorithmStarpu<OctreeClass, ParticleClass, CellClass, RealCellClass, ContainerClass,KernelClass,LeafClass>  AlgorithmClass;
+
+////////////////////////////////////////////////////////////////
+// Main
+////////////////////////////////////////////////////////////////
+
+// Simply create particles and try the kernels
+int main(int argc, char ** argv){
+    ///////////////////////What we do/////////////////////////////
+    std::cout << ">> This executable has to be used to test fmb algorithm.\n";
+    //////////////////////////////////////////////////////////////
+    const int DevP = FParameters::getValue(argc,argv,"-p", 8);
+    const int NbLevels = FParameters::getValue(argc,argv,"-h", 5);
+    const int SizeSubLevels = FParameters::getValue(argc,argv,"-sh", 3);
+    FTic counter;
+    const char* const filename = FParameters::getStr(argc,argv,"-f", "../Data/test20k.fma");
+
+    std::cout << "Opening : " << filename << "\n";
+
+    FFmaLoader<ParticleClass> loader(filename);
+    if(!loader.isOpen()){
+        std::cout << "Loader Error, " << filename << " is missing\n";
+        return 1;
+    }
+
+    // -----------------------------------------------------
+    CellClass::Init(DevP);
+    OctreeClass tree(NbLevels, SizeSubLevels, loader.getBoxWidth(), loader.getCenterOfBox());
+
+    // -----------------------------------------------------
+
+    std::cout << "Creating & Inserting " << loader.getNumberOfParticles() << " particles ..." << std::endl;
+    std::cout << "\tHeight : " << NbLevels << " \t sub-height : " << SizeSubLevels << std::endl;
+    counter.tic();
+
+    loader.fillTree(tree);
+
+    counter.tac();
+    std::cout << "Done  " << "(@Creating and Inserting Particles = " << counter.elapsed() << "s)." << std::endl;
+
+    // -----------------------------------------------------
+
+    KernelClass kernel(DevP, NbLevels,loader.getBoxWidth(), loader.getCenterOfBox());
+    AlgorithmClass algo( &tree, &kernel);
+    std::cout << "There are " << starpu_worker_get_count() << " workers" << std::endl;
+    algo.execute();
+
+    counter.tac();
+    std::cout << "Done  " << "(@Algorithm = " << counter.elapsed() << "s)." << std::endl;
+
+    // -----------------------------------------------------
+
+    { // get sum forces&potential
+        FReal potential = 0;
+        FPoint forces;
+        OctreeClass::Iterator octreeIterator(&tree);
+        octreeIterator.gotoBottomLeft();
+        do{
+            ContainerClass::ConstBasicIterator iter(*octreeIterator.getCurrentListTargets());
+            while( iter.hasNotFinished() ){
+                potential += iter.data().getPotential() * iter.data().getPhysicalValue();
+                forces += iter.data().getForces();
+
+                iter.gotoNext();
+            }
+        } while(octreeIterator.moveRight());
+
+        std::cout << "Foces Sum  x = " << forces.getX() << " y = " << forces.getY() << " z = " << forces.getZ() << std::endl;
+        std::cout << "Potential = " << potential << std::endl;
+    }
+
+    return 0;
+}
