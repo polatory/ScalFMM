154b9598c33c59e21cb4ed15d5ff7ff0e3b5cecb
Berenger Bramas <berenger.bramas@inria.fr>
2012-09-04 13:46:42 +0200

    Print the number of repetition in periodic mode

--- Tests/Kernels/testSphericalEwalAlgorithm.cpp
+++ Tests/Kernels/testSphericalEwalAlgorithm.cpp
@@ -149,6 +149,7 @@ int main(int argc, char ** argv){
     }
     else{
         FmmClass algo(&tree,PeriodicDeep);
+        std::cout << "The simulation box is repeated " << algo.repetitions() << " in X/Y/Z" << std::endl;
         KernelClass kernels( DevP, algo.extendedTreeHeight(), algo.extendedBoxWidth(),algo.extendedBoxCenter());
         algo.setKernel(&kernels);
         algo.execute();
