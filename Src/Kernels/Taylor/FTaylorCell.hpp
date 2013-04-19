a90ba18de624d19bc817bfd6e1fdf9d480b8c1b4
cyrille <cyrille@cyrille-Latitude-D630.(none)>
2013-04-19 11:07:58 +0200

    Ajout d'un repertoire correspondant au Kernel Taylor Expansion, ainsi que de la cellule utilisée

---
+++ Src/Kernels/Taylor/FTaylorCell.hpp
@@ -0,0 +1,35 @@
+// ===================================================================================
+// Copyright ScalFmm 2011 INRIA, Olivier Coulaud, Bérenger Bramas, Matthias Messner
+// olivier.coulaud@inria.fr, berenger.bramas@inria.fr
+// This software is a computer program whose purpose is to compute the FMM.
+//
+// This software is governed by the CeCILL-C and LGPL licenses and
+// abiding by the rules of distribution of free software.  
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public and CeCILL-C Licenses for more details.
+// "http://www.cecill.info". 
+// "http://www.gnu.org/licenses".
+// ===================================================================================
+#ifnedf FTAYLORCELL_HPP
+#define FTAYLORCELL_HPP
+
+#include "../../Components/FBasicCell.hpp"
+
+/**
+ *@author Cyrille Piacibello
+ *@class FTaylorCell
+ *
+ *This class is a cell used for the Taylor Expansion Kernel.
+ *
+ *
+ */
+template <int P>
+class FTaylorCell : public FBasicCell {
+
+
+}
+
+#endif
