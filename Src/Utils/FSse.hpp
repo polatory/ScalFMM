0fd1b39c2431e47f1c8d5dbef26ceb98b3b3cef8
bramas <berenger.bramas@inria.fr>
2013-10-01 16:25:15 +0200

    add intrsec sse functions

--- Src/Utils/FSse.hpp
+++ Src/Utils/FSse.hpp
@@ -1,4 +1,46 @@
 #ifndef FSSE_HPP
 #define FSSE_HPP
 
+#include <xmmintrin.h>  // SSE
+#include <emmintrin.h>  //SSE2
+#include <pmmintrin.h> //SSE3
+#include <tmmintrin.h>  //SSSE3
+#include <smmintrin.h> // SSE4
+
+#ifdef __INTEL_COMPILER
+
+__m128d& operator+=(const __m128d& v1, const __m128d& v2){
+    return (v1 = _mm_add_pd(v1, v2));
+}
+
+__m128d& operator-=(const __m128d& v1, const __m128d& v2){
+    return (v1 = _mm_sub_pd(v1, v2));
+}
+
+__m128d& operator*=(const __m128d& v1, const __m128d& v2){
+    return (v1 = _mm_mul_pd(v1, v2));
+}
+
+__m128d& operator/=(const __m128d& v1, const __m128d& v2){
+    return (v1 = _mm_div_pd(v1, v2));
+}
+
+__m128d operator+(const __m128d& v1, const __m128d& v2){
+    return _mm_add_pd(v1, v2);
+}
+
+__m128d operator-(const __m128d& v1, const __m128d& v2){
+    return _mm_sub_pd(v1, v2);
+}
+
+__m128d operator*(const __m128d& v1, const __m128d& v2){
+    return _mm_mul_pd(v1, v2);
+}
+
+__m128d operator/(const __m128d& v1, const __m128d& v2){
+    return _mm_div_pd(v1, v2);
+}
+
+#endif
+
 #endif // FSSE_HPP
