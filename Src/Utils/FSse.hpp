499701b4aa118977e6c77502ef91451f83c27038
bramas <berenger.bramas@inria.fr>
2013-10-18 19:32:31 +0200

    comment SSE4 header because it is not needed

--- Src/Utils/FSse.hpp
+++ Src/Utils/FSse.hpp
@@ -5,7 +5,7 @@
 #include <emmintrin.h>  //SSE2
 #include <pmmintrin.h> //SSE3
 #include <tmmintrin.h>  //SSSE3
-#include <smmintrin.h> // SSE4
+//#include <smmintrin.h> // SSE4
 
 #ifdef __INTEL_COMPILER
 
