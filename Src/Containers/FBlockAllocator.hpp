6d6ea88d0badf14940b1378faebfb5c391aba7f7
Olivier Coulaud <Olivier.Coulaud@inria.fr>
2014-06-17 11:13:51 +0200

    Remove list include (twice)

--- Src/Containers/FBlockAllocator.hpp
+++ Src/Containers/FBlockAllocator.hpp
@@ -1,5 +1,5 @@
 // ===================================================================================
-// Copyright ScalFmm 2011 INRIA, Olivier Coulaud, Bérenger Bramas, Matthias Messner
+// Copyright ScalFmm 2011 INRIA, Olivier Coulaud, Berenger Bramas, Matthias Messner
 // olivier.coulaud@inria.fr, berenger.bramas@inria.fr
 // This software is a computer program whose purpose is to compute the FMM.
 //
@@ -17,10 +17,9 @@
 #define FBLOCKALLOCATOR_HPP
 
 #include <list>
-#include <list>
 #include <cstring>
 
-#include "../Utils/FAssert.hpp"
+#include "Utils/FAssert.hpp"
 
 /**
  * What a cell allocator should implement
