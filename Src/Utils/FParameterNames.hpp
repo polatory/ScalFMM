569907361109c3b1099db08ba92ba227612e3eed
Olivier Coulaud <Olivier.Coulaud@inria.fr>
2014-11-24 13:49:19 +0100

    Fix typo (?)

--- Src/Utils/FParameterNames.hpp
+++ Src/Utils/FParameterNames.hpp
@@ -1,5 +1,5 @@
 // ===================================================================================
-// Copyright ScalFmm 2011 INRIA, Olivier Coulaud, Bérenger Bramas, Matthias Messner
+// Copyright ScalFmm 2011 INRIA, Olivier Coulaud, Berenger Bramas, Matthias Messner
 // olivier.coulaud@inria.fr, berenger.bramas@inria.fr
 // This software is a computer program whose purpose is to compute the FMM.
 //
@@ -76,7 +76,7 @@ static const FParameterNames InputFileTwow = {
 };
 
 static const FParameterNames InputBinFormat = {
-    {"-binin", "-bininput", "--binary-input"} ,
+    {"-binin", "-bininput", "--binary-input" } ,
      "To input is in binary format."
 };
 
