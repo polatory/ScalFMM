a7f20ece90e5d1dad961830220348b71363dfadc
Berenger Bramas <Berenger.Bramas@inria.fr>
2015-03-12 13:49:27 +0100

    try some other env variable to make it work

--- Tests/noDist/testBlockedWithOpenCLAlgorithm.cpp
+++ Tests/noDist/testBlockedWithOpenCLAlgorithm.cpp
@@ -95,6 +95,10 @@ int main(int argc, char* argv[]){
     setenv("STARPU_NOPENCL","1",1);
     setenv("STARPU_OPENCL_ONLY_ON_CPUS","1",1);
 
+    setenv("STARPU_DISABLE_ASYNCHRONOUS_OPENCL_COPY","1",1);
+    setenv("STARPU_OPENCL_PIPELINE","0",1);
+    setenv("STARPU_OPENCL_ON_CPUS","1",1);
+
     const FParameterNames LocalOptionBlocSize {
         {"-bs"},
         "The size of the block of the blocked tree"
