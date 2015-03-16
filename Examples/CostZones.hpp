0be3112f239b356801eb9d68c9e6b786adb8dbfa
Quentin Khan <quentin.khan@inria.fr>
2015-03-16 15:59:10 +0100

    Refactoring of CostZones + comments

--- Examples/CostZones.hpp
+++ Examples/CostZones.hpp
@@ -1,6 +1,9 @@
 #ifndef _COSTZONES_HPP_
 #define _COSTZONES_HPP_
 
+//#include <utility>
+#include <stdexcept>
+
 template<typename OctreeClass, typename CellClass>
 class CostZones {
     unsigned long long _currentCost = 0;
@@ -52,9 +55,13 @@ private:
      * Counts the children to the left and to the right of the cell currently
      * pointed to by the iterator _it. You must check by yourself whether the
      * cell is a leaf or not.
+     *
+     * \return A pair of int containing the count of left (first) and right
+     *         (second) children.
      */
-    void countLeftRightChildren(int& nbLeftChildren, int& nbRightChildren) {
+    std::pair<int,int> countLeftRightChildren() {
         FCostCell** children = _it.getCurrentChildren();
+        int nbLeftChildren = 0, nbRightChildren = 0; 
         // Left children
         for ( int childIdx = 0; childIdx < 4; childIdx++) {
             if ( children[childIdx] ) {
@@ -66,115 +73,99 @@ private:
             if ( children[childIdx] ) {
                 ++ nbRightChildren;
             }
-        }        
-    }
-
-    void callCostZonesOnChildren(ChildrenSide side, int nbLeftChildren, int nbRightChildren) {
-        int startIdx = (side == LEFT ? 0 : 4);
-        int endIdx   = (side == LEFT ? 4 : 8);
-        
-        int nbChildren = (side == LEFT ? nbLeftChildren : nbRightChildren);
-
-
-        _it.moveDown();
-        
-        // move to the first right child
-        if ( side == RIGHT ) {
-            if ( nbRightChildren == 0)
-                return;
-            
         }
 
+        return std::pair<int,int> (nbLeftChildren, nbRightChildren);
+    }
 
+
+    /**
+     * Calls the costzones function on the left or right children of the current
+     * cell.
+     *
+     * \param side The children side we want to visit.
+     * \param childrenCount The children count as returned by
+     *                      countLeftRightChildren
+     */
+    void callCostZonesOnChildren(const ChildrenSide side, const std::pair<int, int>& childrenCount) {
+        
+        const int& nbChildren = (side == LEFT ? childrenCount.first : childrenCount.second);
+
+        // Don't move if there are no children on the right when we want to
+        // visit them. We test this before moving in case one day moving in the
+        // tree becomes expensive.
+        if ( side == RIGHT && childrenCount.second == 0)
+            return;
+
+        // move down to the children level
+        _it.moveDown();
+        
+        if ( side == RIGHT ) {
+            // move to the first right child
+            for ( int childIdx = 0; childIdx < childrenCount.first; childIdx++) {
+                _it.moveRight();
+            }
+        }
+
+        // Call costzones
+        for ( int childIdx = 0; childIdx < nbChildren; childIdx++ ) {
+            costzones();
+            if(childIdx < nbChildren -1) // nbChildren-1 to avoid changing tree
+                _it.moveRight();
+        }
+
+        // move up to the cell level
         _it.moveUp();
     }
 
+
     void costzones() {
-            // Current position is a leaf
-            CellClass* cell = _it.getCurrentCell();    
 
-            if (cell->_visited) {
-                std::cerr << "Error : cell revisited..." << _it.level()
-                          << ": " << cell->getCoordinate() << std::endl;
-                return;
-            }
-            else
-                cell->_visited = true;
+        CellClass* cell = _it.getCurrentCell();    
 
-            int cellCost = cell->getCost();
-
-            int nbLeftChildren  = 0;
-            int nbRightChildren = 0;
-
+// DEBUG SECTION
 #if 0
-            std::cout << "lvl " << std::setw(2) << _it.level() << " |"
-                      << "cellidx " << std::setw(4)
-                      << cell->getMortonIndex() << " : "
-                      << cell->getCoordinate() << " "
-                      << ( _it.canProgressToDown() ? "internal" : "leaf") << " "
-                      << std::endl;
+        std::cout << "in  lvl " << std::setw(2) << _it.level() << " |"
+                  << "cellidx " << std::setw(4)
+                  << cell->getMortonIndex() << " : "
+                  << cell->getCoordinate() << " "
+                  << ( _it.canProgressToDown() ? "internal" : "leaf") << " "
+                  << std::endl;
+        if (cell->_visited) {
+            std::cerr << "Error : cell revisited..." << _it.level()
+                      << ": " << cell->getCoordinate() << std::endl;
+            throw std::exception();
+            return;
+        } else {
+            cell->_visited = true;
+        }
 #endif
-            // When not on a leaf, apply to left children first
-            if ( _it.canProgressToDown() ) {
+// END DEBUG SECTION
 
-                FCostCell** children = _it.getCurrentChildren();
-                // Count left children
-                for ( int childIdx = 0; childIdx < 4; childIdx++) {
-                    if ( children[childIdx] ) {
-                        ++ nbLeftChildren;
-                    }
-                }
-        
-                _it.moveDown();
-                // Apply costzones to left children
-                for ( int childIdx = 0; childIdx < nbLeftChildren; childIdx++ ) {
-                    costzones();
-                    // avoid changing tree
-                    if(childIdx < nbLeftChildren -1) 
-                        _it.moveRight();
-                }
-                _it.moveUp();
-            }
+        int cellCost = cell->getCost();
+        std::pair<int,int> childrenCount;
 
-            if ( cellCost != 0) {
-                // Add the current cell
-                if ( _currentCost + cellCost < _zones.size() * _totalCost / _nbZones + 1 ) {
-                    _currentCost += cellCost;
-                    _zones.back().push_back({_it.level(), cell});
-                } else {
-                    _zones.push_back(std::vector< std::pair<int, CellClass*> >(1, {_it.level(), cell}));
-                }
-            }
-    
-            // When not on a leaf, apply to right children
-            if ( _it.canProgressToDown() ) {
+        // When not on a leaf, apply to left children first
+        if ( _it.canProgressToDown() ) {
+            childrenCount = countLeftRightChildren();
+            callCostZonesOnChildren(LEFT, childrenCount);
+        }
 
-                FCostCell** children = _it.getCurrentChildren();
-                // Count right children
-                for ( int childIdx = 4; childIdx < 8; childIdx++) {
-                    if ( children[childIdx] ) {
-                        ++ nbRightChildren;
-                    }
-                }
-                
-                if ( nbRightChildren == 0)
-                    return;
-
-                // Move to the first right child
-                _it.moveDown();
-                for ( int childIdx = 0; childIdx < nbLeftChildren; childIdx++) {
-                    _it.moveRight();
-                }
-                // Apply costzones to the right children
-                for ( int childIdx = 0; childIdx < nbRightChildren; childIdx++) {
-                    costzones();
-                    // avoid changing tree
-                     if(childIdx < nbRightChildren -1)
-                        _it.moveRight();
-                }
-                _it.moveUp();
+        if ( cellCost != 0) {
+            // Add the current cell
+            if ( _currentCost + cellCost < _zones.size() * _totalCost / _nbZones + 1 ) {
+                _currentCost += cellCost;
+                _zones.back().push_back({_it.level(), cell});
+            } else {
+                _zones.push_back(std::vector< std::pair<int, CellClass*> >(1, {_it.level(), cell}));
             }
         }
+    
+        // When not on a leaf, apply to right children
+        if ( _it.canProgressToDown() ) {
+            callCostZonesOnChildren(RIGHT, childrenCount);
+        }
+    }
 };
 
 
