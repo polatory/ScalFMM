820462de72c5575e6ddd1d641a331522966bc39c
Olivier Coulaud <Olivier.Coulaud@inria.fr>
2015-03-16 12:02:31 +0100

    Fix problem with convergence criteria  - now the test is ok.

--- UTests/utestSphericalDirect.cpp
+++ UTests/utestSphericalDirect.cpp
@@ -166,8 +166,10 @@ class TestSphericalDirect : public FUTester<TestSphericalDirect> {
 		printf("  Energy DIRECT =   %.12e\n",FMath::Abs(energyD));
 
 		// Assert
-		const FReal MaximumDiffPotential = FReal(9e-3);
-		const FReal MaximumDiffForces     = FReal(9e-2);
+		double epsilon = 1.0/FMath::pow2(ORDER);
+		const FReal MaximumDiffPotential = FReal(epsilon);
+		const FReal MaximumDiffForces     = FReal(10*epsilon);
+		printf(" Criteria error - Epsilon  %e\n",epsilon);
 
 		Print("Test1 - Error Relative L2 norm Potential ");
 		uassert(potentialDiff.getRelativeL2Norm() < MaximumDiffPotential);    //1
