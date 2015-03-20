ca5ba612814ae4621d53fe6233e1db9d011bb68b
Berenger Bramas <Berenger.Bramas@inria.fr>
2015-03-20 18:17:53 +0100

    Add unif grouped test

---
+++ Src/GroupTree/Uniform/FUnifCellPOD.hpp
@@ -0,0 +1,136 @@
+// @SCALFMM_PRIVATE
+#ifndef FUNIFCELLPOD_HPP
+#define FUNIFCELLPOD_HPP
+
+#include "../../Utils/FGlobal.hpp"
+#include "../../Containers/FTreeCoordinate.hpp"
+#include "../StarPUUtils/FStarPUDefaultAlign.hpp"
+#include "../../Kernels/Uniform//FUnifTensor.hpp"
+#include "../../Utils/FComplex.hpp"
+
+struct alignas(FStarPUDefaultAlign::StructAlign) FUnifCellPODCore {
+    MortonIndex mortonIndex;
+    int coordinates[3];
+};
+
+template <int ORDER, int NRHS = 1, int NLHS = 1, int NVALS = 1>
+struct alignas(FStarPUDefaultAlign::StructAlign) FUnifCellPODPole {
+    static const int VectorSize = TensorTraits<ORDER>::nnodes;
+    static const int TransformedVectorSize = (2*ORDER-1)*(2*ORDER-1)*(2*ORDER-1);
+    FReal multipole_exp[NRHS * NVALS * VectorSize]; //< Multipole expansion
+    FComplex transformed_multipole_exp[NRHS * NVALS * TransformedVectorSize];
+};
+
+template <int ORDER, int NRHS = 1, int NLHS = 1, int NVALS = 1>
+struct alignas(FStarPUDefaultAlign::StructAlign) FUnifCellPODLocal {
+    static const int VectorSize = TensorTraits<ORDER>::nnodes;
+    static const int TransformedVectorSize = (2*ORDER-1)*(2*ORDER-1)*(2*ORDER-1);
+    FComplex     transformed_local_exp[NLHS * NVALS * TransformedVectorSize];
+    FReal     local_exp[NLHS * NVALS * VectorSize]; //< Local expansion
+};
+
+template <int ORDER, int NRHS = 1, int NLHS = 1, int NVALS = 1>
+class FUnifCellPOD {
+  FUnifCellPODCore* symb;
+  FUnifCellPODPole<ORDER,NRHS,NLHS,NVALS>* up;
+  FUnifCellPODLocal<ORDER,NRHS,NLHS,NVALS>* down;
+
+public:
+  FUnifCellPOD(FUnifCellPODCore* inSymb, FUnifCellPODPole<ORDER,NRHS,NLHS,NVALS>* inUp,
+            FUnifCellPODLocal<ORDER,NRHS,NLHS,NVALS>* inDown): symb(inSymb), up(inUp), down(inDown){
+  }
+
+  FUnifCellPOD()
+      : symb(nullptr), up(nullptr), down(nullptr){
+  }
+
+
+  /** To get the morton index */
+  MortonIndex getMortonIndex() const {
+      return symb->mortonIndex;
+  }
+
+  /** To set the morton index */
+  void setMortonIndex(const MortonIndex inMortonIndex) {
+      symb->mortonIndex = inMortonIndex;
+  }
+
+  /** To get the position */
+  FTreeCoordinate getCoordinate() const {
+      return FTreeCoordinate(symb->coordinates[0],
+              symb->coordinates[1], symb->coordinates[2]);
+  }
+
+  /** To set the position */
+  void setCoordinate(const FTreeCoordinate& inCoordinate) {
+      symb->coordinates[0] = inCoordinate.getX();
+      symb->coordinates[1] = inCoordinate.getY();
+      symb->coordinates[2] = inCoordinate.getZ();
+  }
+
+  /** To set the position from 3 FReals */
+  void setCoordinate(const int inX, const int inY, const int inZ) {
+      symb->coordinates[0] = inX;
+      symb->coordinates[1] = inY;
+      symb->coordinates[2] = inZ;
+  }
+
+  /** Get Multipole */
+  const FReal* getMultipole(const int inRhs) const
+  {	return up->multipole_exp + inRhs*up->VectorSize;
+  }
+  /** Get Local */
+  const FReal* getLocal(const int inRhs) const{
+    return down->local_exp + inRhs*down->VectorSize;
+  }
+
+  /** Get Multipole */
+  FReal* getMultipole(const int inRhs){
+    return up->multipole_exp + inRhs*up->VectorSize;
+  }
+  /** Get Local */
+  FReal* getLocal(const int inRhs){
+    return down->local_exp + inRhs*down->VectorSize;
+  }
+
+  /** To get the leading dim of a vec */
+  int getVectorSize() const{
+    return down->VectorSize;
+  }
+
+  /** Get Transformed Multipole */
+  const FComplex* getTransformedMultipole(const int inRhs) const{
+    return up->transformed_multipole_exp + inRhs*up->TransformedVectorSize;
+  }
+  /** Get Transformed Local */
+  const FComplex* getTransformedLocal(const int inRhs) const{
+    return down->transformed_local_exp + inRhs*down->TransformedVectorSize;
+  }
+
+  /** Get Transformed Multipole */
+  FComplex* getTransformedMultipole(const int inRhs){
+    return up->transformed_multipole_exp + inRhs*up->TransformedVectorSize;
+  }
+  /** Get Transformed Local */
+  FComplex* getTransformedLocal(const int inRhs){
+    return down->transformed_local_exp + inRhs*down->TransformedVectorSize;
+  }
+
+  /** To get the leading dim of a vec */
+  int getTransformedVectorSize() const{
+    return down->TransformedVectorSize;
+  }
+
+  /** Make it like the begining */
+  void resetToInitialState(){
+    memset(up->multipole_exp, 0, sizeof(FReal) * NRHS * NVALS * up->VectorSize);
+    memset(down->local_exp, 0, sizeof(FReal) * NLHS * NVALS * down->VectorSize);
+    memset(up->transformed_multipole_exp, 0,
+           sizeof(FComplex) * NRHS * NVALS * up->TransformedVectorSize);
+    memset(down->transformed_local_exp, 0,
+           sizeof(FComplex) * NLHS * NVALS * down->TransformedVectorSize);
+  }
+};
+
+#endif // FUNIFCELLPOD_HPP
+
