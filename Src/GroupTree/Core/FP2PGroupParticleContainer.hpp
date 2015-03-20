0b61b0b742ad2c448bd1fc2d8e7d1b0e22fecb53
Berenger Bramas <Berenger.Bramas@inria.fr>
2015-03-20 15:03:43 +0100

    remove not use method for advanced usage of particle container

--- Src/GroupTree/Core/FP2PGroupParticleContainer.hpp
+++ Src/GroupTree/Core/FP2PGroupParticleContainer.hpp
@@ -6,8 +6,8 @@
 
 #include "FGroupAttachedLeaf.hpp"
 
-template<int NRHS = 1, int NLHS = 1>
-class FP2PGroupParticleContainer : public FGroupAttachedLeaf<NRHS, 4*NLHS, FReal> {
+template<int NRHS = 1, int NLHS = 1, int NVALS = 1>
+class FP2PGroupParticleContainer : public FGroupAttachedLeaf<NVALS*NRHS, NVALS*4*NLHS, FReal> {
     typedef FGroupAttachedLeaf<NVALS*NRHS, NVALS*4*NLHS, FReal> Parent;
 
 public:
@@ -18,25 +18,12 @@ public:
 
     }
 
-
     FReal* getPhysicalValues(const int idxVals = 0, const int idxRhs = 0){
       return Parent::getAttribute((0+idxRhs)*NVALS+idxVals);
     }
 
     const FReal* getPhysicalValues(const int idxVals = 0, const int idxRhs = 0) const {
         return Parent::getAttribute((0+idxRhs)*NVALS+idxVals);
-    }
-
-    FReal* getPhysicalValuesArray(const int idxVals = 0, const int idxRhs = 0){
-        return Parent::getRawData() + ((0+idxRhs)*NVALS+idxVals)*Parent::getLeadingRawData();
-    }
-
-    const FReal* getPhysicalValuesArray(const int idxVals = 0, const int idxRhs = 0) const {
-        return Parent::getRawData() + ((0+idxRhs)*NVALS+idxVals)*Parent::getLeadingRawData();
-    }
-
-    int getLeadingDimension(){
-        return Parent::getLeadingRawData();
     }
 
     FReal* getPotentials(const int idxVals = 0, const int idxLhs = 0){
@@ -47,28 +34,12 @@ public:
         return Parent::getAttribute((NRHS+idxLhs)*NVALS+idxVals);
     }
 
-    FReal* getPotentialsArray(const int idxVals = 0, const int idxLhs = 0){
-        return Parent::getRawData() + ((NRHS+idxLhs)*NVALS+idxVals)*Parent::getLeadingRawData();
-    }
-
-    const FReal* getPotentialsArray(const int idxVals = 0, const int idxLhs = 0) const {
-        return Parent::getRawData() + ((NRHS+idxLhs)*NVALS+idxVals)*Parent::getLeadingRawData();
-    }
-
     FReal* getForcesX(const int idxVals = 0, const int idxLhs = 0){
         return Parent::getAttribute((NRHS+NLHS+idxLhs)*NVALS+idxVals);
     }
 
     const FReal* getForcesX(const int idxVals = 0, const int idxLhs = 0) const {
         return Parent::getAttribute((NRHS+NLHS+idxLhs)*NVALS+idxVals);
-    }
-
-    FReal* getForcesXArray(const int idxVals = 0, const int idxLhs = 0){
-        return Parent::getRawData() + ((NRHS+NLHS+idxLhs)*NVALS+idxVals)*Parent::getLeadingRawData();
-    }
-
-    const FReal* getForcesXArray(const int idxVals = 0, const int idxLhs = 0) const {
-        return Parent::getRawData() + ((NRHS+NLHS+idxLhs)*NVALS+idxVals)*Parent::getLeadingRawData();
     }
 
     FReal* getForcesY(const int idxVals = 0, const int idxLhs = 0){
@@ -79,28 +50,12 @@ public:
         return Parent::getAttribute((NRHS+2*NLHS+idxLhs)*NVALS+idxVals);
     }
 
-    FReal* getForcesYArray(const int idxVals = 0, const int idxLhs = 0){
-        return Parent::getRawData() + ((NRHS+2*NLHS+idxLhs)*NVALS+idxVals)*Parent::getLeadingRawData();
-    }
-
-    const FReal* getForcesYArray(const int idxVals = 0, const int idxLhs = 0) const {
-        return Parent::getRawData() + ((NRHS+2*NLHS+idxLhs)*NVALS+idxVals)*Parent::getLeadingRawData();
-    }
-
     FReal* getForcesZ(const int idxVals = 0, const int idxLhs = 0){
         return Parent::getAttribute((NRHS+3*NLHS+idxLhs)*NVALS+idxVals);
     }
 
     const FReal* getForcesZ(const int idxVals = 0, const int idxLhs = 0) const {
         return Parent::getAttribute((NRHS+3*NLHS+idxLhs)*NVALS+idxVals);
-    }
-
-    FReal* getForcesZArray(const int idxVals = 0, const int idxLhs = 0){
-        return Parent::getRawData() + ((NRHS+3*NLHS+idxLhs)*NVALS+idxVals)*Parent::getLeadingRawData();
-    }
-
-    const FReal* getForcesZArray(const int idxVals = 0, const int idxLhs = 0) const {
-        return Parent::getRawData() + ((NRHS+3*NLHS+idxLhs)*NVALS+idxVals)*Parent::getLeadingRawData();
     }
 
     void resetForcesAndPotential(){
