b44e8b717a89d458c3627c4d4b51ea40139f59d6
Martin Khannouz <martin.khannouz@inria.fr>
2016-04-29 10:55:50 +0200

    Change some scripts

--- compile_dag_result.sh
+++ compile_dag_result.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 export SCALFMM_SIMGRIDOUT='scalfmm.out'
-export GROUP_SIZE=50
-export TREE_HEIGHT=5
+export GROUP_SIZE=2000
+export TREE_HEIGHT=6
 export NB_NODE=3
 #export NB_PARTICLE_PER_NODE=$(( (`awk "BEGIN{print 8 ** ($TREE_HEIGHT-1)}"` / $NB_NODE) ))
-export NB_PARTICLE_PER_NODE=50000
-export STARPU_NCPU=1
-export STARPU_STATS=1
-export STARPU_MEMORY_STATS=1
-export STARPU_COMM_STATS=1
+export NB_PARTICLE=2500000
+export STARPU_NCPU=5
+export STARPU_STATS=0
+export STARPU_MEMORY_STATS=0
+export STARPU_COMM_STATS=0
+export STARPU_WATCHDOG_TIMEOUT=10000000 #10s
+export STARPU_WATCHDOG_CRASH=1
 export STARPU_FXT_PREFIX=`pwd`/
 
 echo "GROUP_SIZE=$GROUP_SIZE"
@@ -25,7 +27,7 @@ fi
 test_kernel()
 {
 	#Execute explicit mpi version
-	mpiexec -n $NB_NODE ./Tests/Release/testBlockedMpiAlgorithm -nb $NB_PARTICLE_PER_NODE -bs $GROUP_SIZE -h $TREE_HEIGHT
+	mpiexec -n $NB_NODE ./Tests/Release/testBlockedMpiAlgorithm -nb $NB_PARTICLE -bs $GROUP_SIZE -h $TREE_HEIGHT
 	if [ $? -ne 0 ]; then
 		echo
 		echo " /!\\Error on explicit"
@@ -34,36 +36,35 @@ test_kernel()
 	fi
 
 	#Aggregate task information from explicit execution
-	a=`ls $SCALFMM_SIMGRIDOUT\_*`
-	rm -f $SCALFMM_SIMGRIDOUT
-	for i in $a; do
-		cat $i >> $SCALFMM_SIMGRIDOUT
-		rm -f $i
-	done
+	#a=`ls $SCALFMM_SIMGRIDOUT\_*`
+	#rm -f $SCALFMM_SIMGRIDOUT
+	#for i in $a; do
+		#cat $i >> $SCALFMM_SIMGRIDOUT
+		#rm -f $i
+	#done
 
 	#Get task information
-	cp -f $SCALFMM_SIMGRIDOUT scalfmm_explicit.out
+	#cp -f $SCALFMM_SIMGRIDOUT scalfmm_explicit.out
 
 	#Execute implicit version
-	mpiexec -n $NB_NODE ./Tests/Release/testBlockedImplicitAlgorithm -nb $NB_PARTICLE_PER_NODE -bs $GROUP_SIZE -h $TREE_HEIGHT
+	mpiexec -n $NB_NODE ./Tests/Release/testBlockedImplicitAlgorithm -nb $NB_PARTICLE -bs $GROUP_SIZE -h $TREE_HEIGHT
 	if [ $? -ne 0 ]; then
 		echo
 		echo " /!\\Error on implicit"
 		echo
 		exit
 	fi
-	echo $STARPU_COMM_STATS
 	#Get task information
-	cp -f $SCALFMM_SIMGRIDOUT\_0 scalfmm_implicit.out
-	rm -f $SCALFMM_SIMGRIDOUT\_*
+	#cp -f $SCALFMM_SIMGRIDOUT\_0 scalfmm_implicit.out
+	#rm -f $SCALFMM_SIMGRIDOUT\_*
 
 
 	#Compare DAGs
-	./Tests/Release/compareDAGmapping -e scalfmm_explicit.out -i scalfmm_implicit.out -h $TREE_HEIGHT > output
+	#./Tests/Release/compareDAGmapping -e scalfmm_explicit.out -i scalfmm_implicit.out -h $TREE_HEIGHT > output
 }
 chebyshev_kernel()
 {
-	#mpiexec -n $NB_NODE ./Tests/Release/testBlockedMpiChebyshev -nb $NB_PARTICLE_PER_NODE -bs $GROUP_SIZE -h $TREE_HEIGHT
+	#mpiexec -n $NB_NODE ./Tests/Release/testBlockedMpiChebyshev -nb $NB_PARTICLE -bs $GROUP_SIZE -h $TREE_HEIGHT
 	#if [ $? -ne 0 ]; then
 		#echo
 		#echo " /!\\Error on explicit Chebyshev"
@@ -82,7 +83,7 @@ chebyshev_kernel()
 	##Get task information
 	#cp -f $SCALFMM_SIMGRIDOUT scalfmm_explicit.out
 
-	mpiexec -n $NB_NODE ./Tests/Release/testBlockedImplicitChebyshev -nb $NB_PARTICLE_PER_NODE -bs $GROUP_SIZE -h $TREE_HEIGHT
+	mpiexec -n $NB_NODE ./Tests/Release/testBlockedImplicitChebyshev -nb $NB_PARTICLE -bs $GROUP_SIZE -h $TREE_HEIGHT
 	if [ $? -ne 0 ]; then
 		echo
 		echo " /!\\Error on implicit Chebyshev"
--- export_tarbal.sh
+++ export_tarbal.sh
@@ -8,5 +8,6 @@ cd ..
 rm -rf scalfmm/.git > /dev/null
 tar czf scalfmm.tar.gz scalfmm
 scp scalfmm.tar.gz scm.gforge.inria.fr:/home/groups/scalfmm/htdocs/orgmode/implicit >/dev/null
+ssh scm.gforge.inria.fr "cd /home/groups/scalfmm/htdocs/orgmode/; chmod og+r implicit -R;"
 cd ..
 rm -rf platypus >/dev/null
