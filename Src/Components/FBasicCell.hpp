9269711c75a4a5f39e2eca20739e6c116240084b
Quentin Khan <quentin.khan@inria.fr>
2016-06-30 18:21:12 +0200

    FBasicCell: remove trailing whitespace

--- Src/Components/FBasicCell.hpp
+++ Src/Components/FBasicCell.hpp
@@ -4,13 +4,13 @@
 // This software is a computer program whose purpose is to compute the FMM.
 //
 // This software is governed by the CeCILL-C and LGPL licenses and
-// abiding by the rules of distribution of free software.  
-// 
+// abiding by the rules of distribution of free software.
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public and CeCILL-C Licenses for more details.
-// "http://www.cecill.info". 
+// "http://www.cecill.info".
 // "http://www.gnu.org/licenses".
 // ===================================================================================
 #ifndef FBASICCELL_HPP
@@ -107,5 +107,3 @@ public:
 
 
 #endif //FBASICCELL_HPP
-
-
