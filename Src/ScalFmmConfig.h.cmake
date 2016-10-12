4f50385ba590b5f73953f96518dc94e7eeca8913
Berenger Bramas <bbramas@mpcdf.mpg.de>
2016-10-12 12:30:30 +0200

    reput no scheduler choice in config

--- Src/ScalFmmConfig.h.cmake
+++ Src/ScalFmmConfig.h.cmake
@@ -160,7 +160,7 @@ const std::string SCALFMMCompileLibs("@SCALFMM_COMPILE_LIBS@");
 #cmakedefine SCALFMM_STARPU_USE_COMMUTE
 #cmakedefine SCALFMM_STARPU_USE_REDUX
 #cmakedefine SCALFMM_STARPU_USE_PRIO
-//#cmakedefine SCALFMM_STARPU_FORCE_NO_SCHEDULER
+#cmakedefine SCALFMM_STARPU_FORCE_NO_SCHEDULER
 
 ///////////////////////////////////////////////////////
 // To control simgrid config
