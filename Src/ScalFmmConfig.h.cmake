8cd2d0c8bb5d95ebfda29a33a22493e1f794fe9e
Berenger Bramas <bbramas@mpcdf.mpg.de>
2016-10-12 10:32:35 +0200

    reput prio choice in config

--- Src/ScalFmmConfig.h.cmake
+++ Src/ScalFmmConfig.h.cmake
@@ -159,7 +159,7 @@ const std::string SCALFMMCompileLibs("@SCALFMM_COMPILE_LIBS@");
 
 #cmakedefine SCALFMM_STARPU_USE_COMMUTE
 #cmakedefine SCALFMM_STARPU_USE_REDUX
-//#cmakedefine SCALFMM_STARPU_USE_PRIO
+#cmakedefine SCALFMM_STARPU_USE_PRIO
 //#cmakedefine SCALFMM_STARPU_FORCE_NO_SCHEDULER
 
 ///////////////////////////////////////////////////////
