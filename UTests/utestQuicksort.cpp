f26dbfc15137b498885abe382077957ab7918ad0
Berenger Bramas <bbramas@mpcdf.mpg.de>
2016-10-20 10:03:35 +0200

    Use MPL 2 Licence (similar to LGPL but allow static linking)

--- UTests/utestQuicksort.cpp
+++ UTests/utestQuicksort.cpp
@@ -1,17 +1,16 @@
 // ===================================================================================
-// Copyright ScalFmm 2011 INRIA, Olivier Coulaud, Bérenger Bramas, Matthias Messner
-// olivier.coulaud@inria.fr, berenger.bramas@inria.fr
+// Copyright ScalFmm 2016 INRIA
+//
 // This software is a computer program whose purpose is to compute the FMM.
 //
-// This software is governed by the CeCILL-C and LGPL licenses and
-// abiding by the rules of distribution of free software.  
-// 
+// This software is governed by Mozilla Public License Version 2.0 (MPL 2.0) and
+// abiding by the rules of distribution of free software.
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public and CeCILL-C Licenses for more details.
-// "http://www.cecill.info". 
-// "http://www.gnu.org/licenses".
+// Mozilla Public License Version 2.0 (MPL 2.0) for more details.
+// https://www.mozilla.org/en-US/MPL/2.0/
 // ===================================================================================
 #include "FUTester.hpp"
 #include "Utils/FQuickSort.hpp"
